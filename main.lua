a326e5626fa73ad94b22072396afd73f62d29987
Sebastian N <sebastian.navas@ocsbstudent.ca>
2018-02-08 13:57:19 -0500

    addition of display

--- main.lua
+++ main.lua
@@ -7,5 +7,5 @@
 --
 --This program says Hello World!
 -----------------------------------------------------------------------------------------
-
 print("Hello World!")
+local mytext = display.newText( "Hello World!", 600, 600, native.SystemFont, 100)
